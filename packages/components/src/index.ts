5ee3d01c9d9c35ab8471e2ec1805ccea2f996f7b
xonecas <seancaetanomartin@gmail.com>
2026-04-05 15:04:24 +0100

    docs: update components module doc, regenerate API docs

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -6,7 +6,7 @@
  *
  * @example
  * ```ts
- * import { Spacer, Divider, Button, Select } from "@cel-tui/components";
+ * import { Spacer, Divider, VDivider, Button, Select, Markdown } from "@cel-tui/components";
  *
  * HStack({ height: 1 }, [
  *   Text("Title", { bold: true }),
